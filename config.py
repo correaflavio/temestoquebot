885772ca343460d15523d4e1a46f4c12a3b9d4b5
Flavio Correa <flcorrea@cisco.com>
2019-08-12 18:18:51 -0300

    setup

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # Ponha aqui os dados do seu robozinho
-bottoken='YTg5YjMwODMtYjliMS00YjM3LTlmMjYtNzlhZWU1MDE0NDlhODIwYzMwODctZDVj_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f'
-botmail="partnerbotbr@webex.bot"
+bottoken='OTQ5MTUzNGMtYjBjNS00MGZmLWE0NTUtODU3MDYwYWNmYzY1YzVmNDZhYmYtZGQ5_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f'
+botmail="temestoque@webex.bot"
 
 # Ponha aqui os dados do seu Webhook
 # webhook_url o endere�o publico onde est� o app
-webhook_url="https://partnerbotbr.herokuapp.com"
+webhook_url="https://temestoquebot.herokuapp.com"
 # webhook_name e' o nome do gatilho que o Webex Teams gera para seu aplicativo entender
-webhook_name="partnerbotbr"
+webhook_name="temestoque"

c36df45784ef29af33e7fc83bd51aa96bc513b5c
Flavio Correa <flcorrea@cisco.com>
2019-08-12 18:22:47 -0300

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ botmail="temestoque@webex.bot"
 
 # Ponha aqui os dados do seu Webhook
 # webhook_url o endere�o publico onde est� o app
-webhook_url="https://temestoquebot.herokuapp.com"
+webhook_url="https://temestoque.herokuapp.com"
 # webhook_name e' o nome do gatilho que o Webex Teams gera para seu aplicativo entender
 webhook_name="temestoque"

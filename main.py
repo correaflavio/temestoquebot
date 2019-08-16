e73f2d5ce2896e9ea7ab017b673a591a1f778bce
Flavio Correa <flcorrea@cisco.com>
2019-08-15 23:46:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -69,12 +69,12 @@ def trataPOST(content):
             webexmsgRoomviaID(sala,msg)
             
             # Envia log de uso para outra sala
-            sala_log = getwebexRoomID(log_bot)
-            print (sala_log)
-            log_room_id = "Y2lzY29zcGFyazovL3VzL1JPT00vODhhYzFiODAtYmRiZC0xMWU5LWI3NjEtN2Y4ZjU4YzU1MGFj"
-            print (log_room_id)
-            msg_log = "bot: Tem Estoque |" + "user:" + usermail + "| comando:" + mensagem
-            print (msg_log)
+            #sala_log = getwebexRoomID(log_bot)
+            #print (sala_log)
+            #log_room_id = "Y2lzY29zcGFyazovL3VzL1JPT00vODhhYzFiODAtYmRiZC0xMWU5LWI3NjEtN2Y4ZjU4YzU1MGFj"
+            #print (log_room_id)
+            #msg_log = "bot: Tem Estoque |" + "user:" + usermail + "| comando:" + mensagem
+            #print (msg_log)
             #webexmsgRoomviaID(sala_log,msg_log)
 
     except:

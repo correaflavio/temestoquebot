b024baf74567eccf8d2761affacab3b96a935f67
Flavio Correa <flcorrea@cisco.com>
2019-08-19 12:30:16 -0300

    Update funcoes_Cisco.py

--- funcoes_Cisco.py
+++ funcoes_Cisco.py
@@ -72,7 +72,7 @@ def log_bot_smartsheet():
     #URL smartsheet para adicionar linhas 
     url = "https://api.smartsheet.com/2.0/sheets/"+ smartsheet_bot_log_id + "/rows"
     print (url)
-    payload = {"toTop": True, "cells": [{"columnId": 1904224842868612, "value": bot_name}, {"columnId": 6407824470239108, "value": user_id}, {"columnId": 4156024656553860, "value": comando}]}
+    payload = {"cells": [{"columnId": 1904224842868612, "value": bot_name}, {"columnId": 6407824470239108, "value": user_id}, {"columnId": 4156024656553860, "value": comando}]}
 
     #payload = {\"toTop\":true, \"cells\": [ {\"columnId\": 1904224842868612, \"value\": \"bot_name\"}, {\"columnId\": 6407824470239108, \"value\": \"user_id\", {\"columnId\": 4156024656553860, \"value\": \"comando\"}] }
     print (payload)
